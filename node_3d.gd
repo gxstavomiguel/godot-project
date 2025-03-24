7ba62e1e0b46fdaf736a2dd03c4b8125532c5e94
Gustavo Miguel <gustavomiguelsilvafc@gmail.com>
2025-03-24 20:18:16 -0300

    Câmera em primeira pessoa

--- node_3d.gd
+++ node_3d.gd
@@ -1,25 +1,9 @@
 extends Node3D
 
-@export var variavel : int = 1
-@onready var decimal: float = 0.0
-var boleano: bool = false
-var text : String = 'texto'
-
-var vector2 : Vector2 = Vector2.ZERO
-var vector3: Vector3 = Vector3.ZERO
-var vector4: Vector4 = Vector4.ZERO
-
-var array : Array[String] = ["txt1", "txt2"]
-
-var dicionario : Dictionary = {}
-
-@onready var ball: RigidBody3D = $Ball
-
 func _ready():
 	pass
 	
 func _process(delta):
 	var direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	if direcao.length() > 0:
-		ball.apply_impulse(Vector3(direcao.y, 0.0, -direcao.x).normalized() * 0.5)
+
+pass
--- player.gd
+++ player.gd
@@ -1,10 +1,24 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 8.0
+const JUMP_VELOCITY = 6.5
+const MOUSE_SENSITIVITY = 0.01
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var pivot = $Pivot
+@onready var camera = $Pivot/Camera3D
 
+func _input(event):
+	if event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif Input.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		pivot.rotate_y(-(event as InputEventMouseMotion).relative.x * MOUSE_SENSITIVITY)
+		camera.rotate_x(-(event as InputEventMouseMotion).relative.y * MOUSE_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+		
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -=  gravity * delta
@@ -12,8 +26,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	var input_dir = Input.get_vector("down", "left", "right", "forward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "down", "forward" , "right", )
+	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
